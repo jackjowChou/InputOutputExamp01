1e1fb06a44dc903c476449d46653b80b14b54646
jackjowchou <yungchen@asia.edu.tw>
2016-09-03 17:00:15 +0800

    Adding two values function finish
    Adding two values function finish

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int v1 = scn.nextInt();
+		int v2 = scn.nextInt();
+		System.out.println(v1 + " + " + v2 + " = " + (v1 + v2));
 	}
 
 }
